351bee02e357fe4ffe0c53543dbaafb931f5f73d
Mandrea Radu Alexandru <radu.mandrea15@gmail.com>
2023-11-01 23:19:42 +0200

    v1.1

--- opt_contest_1/src/main.rs
+++ opt_contest_1/src/main.rs
@@ -1,17 +1,17 @@
-
 use std::fs;
 use std::io;
 
 fn rot13() -> Result<(), io::Error> {
-
     let s = fs::read_to_string("input.txt")?;
     let mut result: String = String::new();
     let mut ch;
     for c in s.chars() {
-
         ch = c as u8;
-        if !c.is_ascii(){
-            return Err(io::Error::new(io::ErrorKind::InvalidData, "Char encountered is not ASCII"));
+        if !c.is_ascii() {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Char encountered is not ASCII",
+            ));
         } else {
             if ch >= 'a' as u8 && ch <= 'm' as u8 {
                 ch = (ch as u32 + 13) as u8;
@@ -22,25 +22,24 @@ fn rot13() -> Result<(), io::Error> {
             } else if ch >= 'A' as u8 && ch <= 'M' as u8 {
                 ch = (ch as u32 + 13) as u8;
                 result.push(ch as char);
-            } else  if ch >= 'N' as u8 && ch <= 'Z' as u8{
+            } else if ch >= 'N' as u8 && ch <= 'Z' as u8 {
                 ch = ('A' as u32 + (ch as u32 + 12) % ('Z' as u32)) as u8;
                 result.push(ch as char);
-            }
-            else {  //am orice ascii in loc de litere (nu cast ch la u8 pt a nu pierde biti)
+            } else {
+                //am orice ascii in loc de litere (nu cast ch la u8 pt a nu pierde biti)
                 result.push(c);
             }
         }
     }
     fs::write("output.txt", result)?;
     Ok(())
-    //return result;
 }
 fn main() {
     //Ç███ÆÆ (caracterele sunt ASCII dar vad ca nu mi le vede drept ASCII analizatorul idk dc)
 
-    if let Err(err) = rot13(){
+    if let Err(err) = rot13() {
         eprintln!("Error: {:?}", err);
-    }else {
+    } else {
         println!("Done!");
     }
 }
