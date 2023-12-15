c0ab0bedc2e842a84bca6638a80914d86e40a53c
RADUMAN15 <radu.mandrea15@gmail.com>
2023-12-15 02:30:48 +0200

    GUI list view (fara global mem)

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,11 +1,20 @@
+use druid::widget::Button;
+use druid::widget::SizedBox;
+use druid::widget::{Align, Flex, Label, Scroll};
+use druid::WidgetExt;
+use druid::{AppLauncher, Data, Lens, LocalizedString, Widget, WindowDesc};
 use std::collections::HashMap;
 use std::fs;
 use std::io;
 
+const VERTICAL_WIDGET_SPACING: f64 = 20.0;
+const WINDOW_TITLE: LocalizedString<AppState> = LocalizedString::new("Task Mandanger");
+//const HORIZONTAL_WIDGET_SPACING: f64 = 20.0;
+
 #[derive(Debug, Clone)]
 struct ProcessData {
-    pid : u32,
-    ppid : u32,
+    pid: u32,
+    ppid: u32,
     name: String,
     cpu_used: u32,
     memory_used: u32,
@@ -14,8 +23,8 @@ struct ProcessData {
 }
 trait Methods {
     fn new(
-        pid : u32,
-        ppid : u32,
+        pid: u32,
+        ppid: u32,
         name: String,
         cpu_used: u32,
         memory_used: u32,
@@ -23,10 +32,11 @@ trait Methods {
         author: String,
     ) -> Self;
 }
+
 impl Methods for ProcessData {
     fn new(
-        pidi : u32,
-        ppidi : u32,
+        pidi: u32,
+        ppidi: u32,
         namei: String,
         cpu_usedi: u32,
         memory_usedi: u32,
@@ -34,9 +44,8 @@ impl Methods for ProcessData {
         authori: String,
     ) -> Self {
         ProcessData {
-
-            pid : pidi,
-            ppid : ppidi,
+            pid: pidi,
+            ppid: ppidi,
             name: namei,
             cpu_used: cpu_usedi,
             memory_used: memory_usedi,
@@ -45,14 +54,104 @@ impl Methods for ProcessData {
         }
     }
 }
-fn main() {
 
+#[derive(Clone, Data, Lens)]
+struct AppState {
+    view_name: String,
+    view_option: bool,
+    process_info: String,
+    global_cpu: String,
+    global_mem: String,
+}
+
+fn build_root_widget() -> impl Widget<AppState> {
+    let button = Button::new("Change ViewMode")
+        .on_click(|_ctx, data: &mut AppState, _env| {
+            data.view_option = !data.view_option;
+            if data.view_option {
+                data.view_name = "Tree".to_string();
+                data.process_info = "PROCESS IN TREE MODE\n".to_string();
+            } else {
+                data.view_name = "List".to_string();
+                data.process_info = printproc().unwrap();
+            }
+            data.global_cpu = printglobal().unwrap();
+            data.global_mem = printglobal().unwrap();
+        })
+        .fix_width(200.0)
+        .fix_height(50.0);
+
+    let label_view_mode = Label::new(|data: &AppState, _env: &_| {
+        // Fix: Use the name field from the AppState.
+        format!("ViewMode: {}", data.view_name)
+    });
+
+    let label_processes =
+        Label::new(|data: &AppState, _env: &_| data.process_info.clone()).with_text_size(18.0);
+    let scrollable_label_processes = Scroll::new(label_processes);
+
+    let sized_scroll = SizedBox::new(scrollable_label_processes)
+        .expand_height()
+        .height(500.0)
+        .expand_width()
+        .width(400.0); // Wrap the Label in a Scroll widget to make it scrollable
+
+    let label_global_cpu: Label<AppState> = Label::new(|data: &AppState, _env: &_| {
+        // Fix: Use the name field from the AppState.
+        format!("{}", data.global_cpu)
+    })
+    .with_text_size(18.0);
+
+    let label_global_mem: Label<AppState> = Label::new(|data: &AppState, _env: &_| {
+        // Fix: Use the name field from the AppState.
+        format!("{}", data.global_mem)
+    })
+    .with_text_size(18.0);
+
+    // arrange the two widgets vertically, with some padding
+    let change_view_layout = Flex::column()
+        .with_child(label_view_mode)
+        .with_spacer(1.0 * VERTICAL_WIDGET_SPACING)
+        .with_child(button)
+        .with_spacer(16.0 * VERTICAL_WIDGET_SPACING)
+        .with_child(label_global_cpu)
+        .with_spacer(1.0 * VERTICAL_WIDGET_SPACING)
+        .with_child(label_global_mem);
+    let layout2 = Flex::row()
+        .with_spacer(10.0)
+        .with_child(sized_scroll)
+        .with_flex_spacer(50.0)
+        .with_child(change_view_layout)
+        .with_spacer(15.0);
+
+    // center the two widgets in the available space
+    Align::centered(layout2)
+}
+fn main() {
     //K = PID, V = LIST OF SUBPROCESSES
     //let mut _process_list : HashMap<u32, Vec<ProcessData>> = HashMap::new();
 
     printproc().unwrap();
 
     printglobal().unwrap();
+
+    let main_window = WindowDesc::new(build_root_widget())
+        .title(WINDOW_TITLE)
+        .window_size((800.0, 600.0));
+
+    // create the initial app state
+    let initial_state = AppState {
+        view_name: "List".into(),
+        view_option: false,
+        process_info: "".into(),
+        global_cpu: "".into(),
+        global_mem: "".into(),
+    };
+
+    // start the application
+    AppLauncher::with_window(main_window)
+        .launch(initial_state)
+        .expect("Failed to launch application");
 }
 fn get_username(uid: u32) -> Result<String, io::Error> {
     let passwd_file_path: &str = "/etc/passwd";
@@ -75,11 +174,10 @@ fn get_username(uid: u32) -> Result<String, io::Error> {
     ))
 }
 
-fn printproc() -> Result<HashMap<u32, Vec<u32>>, io::Error> {
-
+fn printproc() -> Result<String, io::Error> {
     let mut processes: Vec<ProcessData> = Vec::new();
 
-    let mut _process_list : HashMap<u32, Vec<u32>> = HashMap::new();
+    let mut _process_list: HashMap<u32, Vec<u32>> = HashMap::new();
 
     let paths: fs::ReadDir = fs::read_dir("/proc").unwrap();
     for path in paths {
@@ -93,15 +191,8 @@ fn printproc() -> Result<HashMap<u32, Vec<u32>>, io::Error> {
 
                 let mut uid: u32 = 0;
 
-                let mut add: ProcessData = ProcessData::new(
-                    0,
-                    0,
-                    "".to_string(),
-                    0,
-                    0,
-                    "".to_string(),
-                    "".to_string(),
-                );
+                let mut add: ProcessData =
+                    ProcessData::new(0, 0, "".to_string(), 0, 0, "".to_string(), "".to_string());
                 for line in proc_status.lines() {
                     if line.contains("Name") {
                         //println!("{line}");
@@ -206,10 +297,9 @@ fn printproc() -> Result<HashMap<u32, Vec<u32>>, io::Error> {
     }
     //println!("\n{:?}\n",processes);
 
-    for process in &processes{
-
-        let mut children : Vec<u32> = Vec :: new();
-        for ischild in &processes{
+    for process in &processes {
+        let mut children: Vec<u32> = Vec::new();
+        for ischild in &processes {
             if ischild.ppid == process.pid {
                 children.push(ischild.pid);
             }
@@ -225,28 +315,45 @@ fn printproc() -> Result<HashMap<u32, Vec<u32>>, io::Error> {
     for (_, values) in sorted_vec.iter_mut() {
         values.sort();
     }
+    let mut output: String = String::new();
+    // for (key, value) in sorted_vec {
 
-    for (key, value) in sorted_vec {
-        println!("PID: {}, SUB-PIDS: {:?}", key, value);
-    }
+    //     println!("PID: {}, SUB-PIDS: {:?}", key, value);
+    // }
 
     //println!("{:?}", _process_list);
-    let mut _process_list : HashMap<u32, Vec<u32>> = HashMap::new();
-    Ok(_process_list)
+    processes.sort_by_key(|proc| proc.pid);
+
+    for proc in &processes {
+        output.push_str("Name: ");
+        output.push_str(&proc.name);
+        output.push_str("\nCpu used: ");
+        output.push_str(&proc.cpu_used.to_string());
+        output.push_str("%\nMemory used: ");
+        output.push_str(&proc.memory_used.to_string());
+        output.push_str(" Kb\nPath: ");
+        if proc.path.len() == 0 {
+            output.push_str(".");
+        } else {
+            output.push_str(&proc.path);
+        }
+        output.push_str("\nUsername: ");
+        output.push_str(&proc.author);
+        output.push_str("\n\n");
+    }
+    Ok(output)
 }
 
-fn printglobal() -> Result<(), io::Error> {
-
+fn printglobal() -> Result<String, io::Error> {
     let global_proc_stat_path: String = String::from("/proc/stat");
     let processor_stat: String = fs::read_to_string(global_proc_stat_path)?;
 
     let line = processor_stat.lines().next().unwrap();
 
-    let args : Vec<&str> = line.split_whitespace().collect();
-    let mut sum : u32 = 0;
-    for arg in &args{
-
-        if arg.parse::<u32>().is_ok(){
+    let args: Vec<&str> = line.split_whitespace().collect();
+    let mut sum: u32 = 0;
+    for arg in &args {
+        if arg.parse::<u32>().is_ok() {
             let value = arg.parse::<u32>().unwrap();
             sum += value;
         }
@@ -255,7 +362,11 @@ fn printglobal() -> Result<(), io::Error> {
     let idle_u32 = idle_str.parse::<u32>().unwrap();
 
     let global_usage = (100 * idle_u32) / sum;
-    println!("CPU USAGE: {:?}%",global_usage);
+    // println!("CPU USAGE: {}%",global_usage);
 
-    Ok(())
+    let mut out = String::from("GLOBAL CPU USAGE: ");
+    out.push_str(&global_usage.to_string());
+    out.push_str("%");
+
+    Ok(out)
 }
